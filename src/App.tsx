95a09c7e2407e293c0eb491b66ac55a29e79ae94
Alexander Konkov <alxndr.konkov@gmail.com>
2024-01-24 00:35:49 +0300

    Added images

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import WebApp from '@twa-dev/sdk';
 
 function App() {
   const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
@@ -16,18 +16,30 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Здесь скоро будет открыт магазин!</h1>
+      <div className='products-list'>
+        <div className="product">
+          <img src="https://nevestam.by/files/novosti-2022/prozrachnoe-platie-001.png" alt="" />
+          <h2>Платье "Винтаж"</h2>
+          <button>Купить</button>
+        </div>
+        <div className="product">
+          <img src="https://shop-cdn1-2.vigbo.tech/shops/17822//products/22114993/images/preview-767a927194dc6646b8d4acd68942f02e.jpeg" alt="" />
+          <h2>Платье "Шантаж"</h2>
+          <button>Купить</button>
+        </div>
+        <div className="product">
+          <img src="https://june.by/image/cache/catalog/Vechernia/1-2480x3508.jpg" alt="" />
+          <h2>Платье "Кураж"</h2>
+          <button>Купить</button>
+        </div>
+
+      </div>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          Кнопка {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
