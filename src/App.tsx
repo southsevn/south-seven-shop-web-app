82d4febb0494c705df7adb8a6cb6aa6b7f89d71d
Alexander Konkov <alxndr.konkov@gmail.com>
2024-01-24 01:05:47 +0300

    Style changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,11 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Здесь скоро будет открыт магазин!</h1>
+      <h1>Abnova</h1>
       <div className='products-list'>
         <div className="product">
           <img src="https://nevestam.by/files/novosti-2022/prozrachnoe-platie-001.png" alt="" />
